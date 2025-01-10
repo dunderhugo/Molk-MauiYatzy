328495e75592fdb072b5c161b95876e7e5c70470
Hugo Forssell <147729983+dunderhugo@users.noreply.github.com>
2025-01-10 09:03:44 +0100

    Initial commit

---
+++ Maui-Yatzy/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Maui_Yatzy
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
