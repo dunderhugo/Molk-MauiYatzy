3859c75190a4f5e82a7277d26253f14670043306
Hugo Forssell <147729983+dunderhugo@users.noreply.github.com>
2025-01-16 13:21:33 +0100

    Main menu layout fixed
    Added score to player class

--- Maui-Yatzy/Components/Classes/Player.cs
+++ Maui-Yatzy/Components/Classes/Player.cs
@@ -6,19 +6,62 @@ using System.Threading.Tasks;
 
 namespace Maui_Yatzy.Components.Classes
 {
-    internal class Player
-    {
-        string Name { get; set; }
-        private int Score { get; set; }
-        public Player(string name)
-        {
-            Name = name;
-            Score = 0;
-        }
+	internal class Player
+	{
+		public string Name { get; private set; }
+		public Dictionary<ScoreCategory, int> ScoreList { get; private set; }
 
-        public static List<int> ScoreList = new List<int>();
-        public override string ToString() => $"{Name}";
+		public Player(string name)
+		{
+			Name = name;
+			ScoreList = InitializeScoreList();
+		}
 
-        public static List<Player>? PlayerList { get; set; } = new List<Player>();
-    }
+		private Dictionary<ScoreCategory, int> InitializeScoreList()
+		{
+			var scores = new Dictionary<ScoreCategory, int>();
+			foreach (ScoreCategory category in Enum.GetValues(typeof(ScoreCategory)))
+			{
+				scores[category] = 0;
+			}
+			return scores;
+		}
+
+		public void UpdateScore(ScoreCategory category, int score)
+		{
+			if (ScoreList.ContainsKey(category))
+			{
+				ScoreList[category] = score;
+			}
+		}
+		public int TotalScore => CalculateTotalScore();
+
+		private int CalculateTotalScore()
+		{
+			int total = 0;
+			foreach (var score in ScoreList.Values)
+			{
+				total += score;
+			}
+			return total;
+		}
+		public static List<Player> PlayerList { get; private set; } = new List<Player>();
+		public override string ToString() => $"{Name}";
+	}
+	public enum ScoreCategory
+	{
+		Ones,
+		Twos,
+		Threes,
+		Fours,
+		Fives,
+		Sixes,
+		ThreeOfAKind,
+		FourOfAKind,
+		FullHouse,
+		SmallStraight,
+		LargeStraight,
+		Yatzy,
+		Chance
+	}
 }
--- Maui-Yatzy/MainPage.xaml.cs
+++ Maui-Yatzy/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Media;
 namespace Maui_Yatzy
 {
     public partial class MainPage : ContentPage
